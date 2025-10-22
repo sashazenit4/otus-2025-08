5eddb408001eec65235cd46f083545cd7e73bf61
Aleksandr.KHolin <Aleksandr.KHolin@norbit.ru>
2025-10-22 21:33:55 +0300

    feat(lecture): implement module with homework example

---
+++ local/modules/aholin.crmcustomtab/.settings.php
@@ -0,0 +1,33 @@
+<?php
+
+use Bitrix\Main\DI\ServiceLocator;
+use Bitrix\Main\UserTable;
+use Otus\Entities\Main\UserController;
+use Otus\Entities\Main\UserRepository;
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
+
+return [
+    'controllers' => [
+        'value' => [
+            'defaultNamespace' => '\\Aholin\\Crmcustomtab\\Controller',
+        ],
+        'readonly' => true,
+    ],
+    'services' => [
+        'value' => [
+            'user.controller' => [
+                'constructor' => static function () {
+                    $userRepository = ServiceLocator::getInstance()->get('user.repository');
+                    return new UserController($userRepository);
+                }
+            ],
+            'user.repository' => [
+                'constructor' => static function () {
+                    return new UserRepository(UserTable::class);
+                },
+            ],
+        ],
+    ],
+];
---
+++ local/modules/aholin.crmcustomtab/lib/Helper.php
@@ -0,0 +1,10 @@
+<?php
+namespace Aholin\Crmcustomtab;
+
+class Helper
+{
+    public static function helloWorld(): string
+    {
+        return 'Hello world!';
+    }
+}
